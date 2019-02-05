ee37687cf07e4c4bd64d9b70bf3ff34bacef1229
Guillaume Briday <guillaumebriday@gmail.com>
2019-02-05 14:00:14 +0100

    --wip-- [skip ci]

--- src/components/PostListing.jsx
+++ src/components/PostListing.jsx
@@ -15,7 +15,7 @@ export default ({ posts }) => (
         <span itemProp="articleSection" className="bg-indigo-lightest text-indigo py-1 px-4 rounded-full text-xs font-semibold">{post.frontmatter.category}</span>
 
         <h2 className="font-semibold m-0">
-          <Link to={post.fields.slug}>
+          <Link to={post.fields.slug} className="text-grey-darkest">
             <span itemProp="name">{post.frontmatter.title}</span>
           </Link>
         </h2>
