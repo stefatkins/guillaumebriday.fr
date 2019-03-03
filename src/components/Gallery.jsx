c3f3c4af16d19e4668eab8db33c49e7bb601762a
Guillaume Briday <guillaumebriday@gmail.com>
2019-03-03 23:54:01 +0100

    --wip-- [skip ci]

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,64 +5,64 @@ import Img from 'gatsby-image'
 
 class Gallery extends Component {
   constructor() {
-    super();
+    super()
 
     this.state = {
       lightboxIsOpen: false,
       currentImage: 0,
-    };
+    }
 
-    this.closeLightbox = this.closeLightbox.bind(this);
-    this.gotoNext = this.gotoNext.bind(this);
-    this.gotoPrevious = this.gotoPrevious.bind(this);
-    this.gotoImage = this.gotoImage.bind(this);
-    this.handleClickImage = this.handleClickImage.bind(this);
-    this.openLightbox = this.openLightbox.bind(this);
+    this.closeLightbox = this.closeLightbox.bind(this)
+    this.gotoNext = this.gotoNext.bind(this)
+    this.gotoPrevious = this.gotoPrevious.bind(this)
+    this.gotoImage = this.gotoImage.bind(this)
+    this.handleClickImage = this.handleClickImage.bind(this)
+    this.openLightbox = this.openLightbox.bind(this)
   }
 
   openLightbox(index, event) {
-    event.preventDefault();
+    event.preventDefault()
     this.setState({
       currentImage: index,
       lightboxIsOpen: true,
-    });
+    })
   }
 
   closeLightbox() {
     this.setState({
       currentImage: 0,
       lightboxIsOpen: false,
-    });
+    })
   }
 
   gotoPrevious() {
     this.setState({
       currentImage: this.state.currentImage - 1,
-    });
+    })
   }
 
   gotoNext() {
     this.setState({
       currentImage: this.state.currentImage + 1,
-    });
+    })
   }
 
   gotoImage(index) {
     this.setState({
       currentImage: index,
-    });
+    })
   }
 
   handleClickImage() {
-    if (this.state.currentImage === this.props.images.length - 1) return;
+    if (this.state.currentImage === this.props.images.length - 1) return
 
-    this.gotoNext();
+    this.gotoNext()
   }
 
   renderGallery() {
-    const { images } = this.props;
+    const { images } = this.props
 
-    if (!images) return;
+    if (!images) return
 
     const gallery = images.map((obj, i) => {
       return (
@@ -78,10 +78,8 @@ class Gallery extends Component {
     })
 
     return (
-      <div className="row">
-        {gallery}
-      </div>
-    );
+      {gallery}
+    )
   }
   render() {
     return (
@@ -105,13 +103,13 @@ class Gallery extends Component {
           width="auto"
         />
       </div>
-    );
+    )
   }
 }
 
-Gallery.displayName = 'Gallery';
+Gallery.displayName = 'Gallery'
 Gallery.propTypes = {
   images: PropTypes.array
-};
+}
 
-export default Gallery;
+export default Gallery
